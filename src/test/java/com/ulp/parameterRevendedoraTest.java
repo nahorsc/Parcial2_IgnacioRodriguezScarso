c4d8e566cb67a8f7397067fbe67de6803dd59405
Ignacio Rodriguez Scarso <nacho.rodriguez162@gmail.com>
2026-06-13 15:30:00 -0300

    Test parametrizado

--- src/test/java/com/ulp/parameterRevendedoraTest.java
+++ src/test/java/com/ulp/parameterRevendedoraTest.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.ulp;
 
 /**
  *
  * @author nacho
  */
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class parameterRevendedoraTest {
-    
+
+    private static revendedora cRev = new revendedora(400, 9000.0);
+
+    public static Collection<Object[]> tomarDatos() {
+        return Arrays.asList(new Object[][]{
+            {300.5, cRev, 3333.0},
+            {500.0, cRev, 8199.5},
+            {9999.0, cRev, 1111.0}
+        });
+    }
+
+    @ParameterizedTest
+    @MethodSource("tomarDatos")
+    public void testRetirarDinero(double montoActual, revendedora cuenta, double montoEsperado) throws Exception {
+
+        System.out.println("Test Retiros");
+        System.out.println("Saldo inicial:  $" + cuenta.getSaldo());
+        System.out.println("Intentamos retirar $" + montoActual);
+
+        try {
+            cuenta.retirarDinero(montoActual);
+
+            System.out.println("Saldo final $" + cuenta.getSaldo());
+
+            assertEquals(montoEsperado, cuenta.getSaldo(), 0.2);
+
+        } finally {
+            System.out.println("Operacion terminada");
+        }
+    }
 }
+
